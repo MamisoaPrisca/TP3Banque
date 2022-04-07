38a28827bf9f3b812773b0924af923e51628f693
Ny Anjara Mamisoa Prisca <nyampris@gmail.com>
2022-04-07 16:06:11 +0300

    Écriture d'une entité CompteBancaire

---
+++ src/main/java/mg/nyanjaramamisoa/tp3banque/resources/JakartaEE8Resource.java
@@ -0,0 +1,20 @@
+package mg.nyanjaramamisoa.tp3banque.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("rest")
+public class JakartaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
